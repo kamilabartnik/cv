b64084cd00fbedf3b6a832b8a47bd4b444c0a5dc
kamilabartnik <kbartnik@g.pl>
2019-10-31 16:19:14 +0100

    react html nav deploy

---
+++ build/precache-manifest.9d1c5c210fa91b819caef98f6923a14f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4995ba5b0071494734c675b68e8760",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "4fbf79730ce828656893",
+    "url": "/cv/static/css/main.7a38a366.chunk.css"
+  },
+  {
+    "revision": "016fc5eb8cfa727892fe",
+    "url": "/cv/static/js/2.84e5f926.chunk.js"
+  },
+  {
+    "revision": "4fbf79730ce828656893",
+    "url": "/cv/static/js/main.6fa7010d.chunk.js"
+  },
+  {
+    "revision": "a1ec7a6a8174e7aa9843",
+    "url": "/cv/static/js/runtime-main.d48d6d87.js"
+  }
+]);
