cf7604433e76c715f7df2ebd979db8d830e4ee28
kamilabartnik <kbartnik@g.pl>
2020-01-13 12:32:27 +0100

    Updates

--- precache-manifest.988f06a753d096aa7746acc50cb578e9.js
+++ precache-manifest.988f06a753d096aa7746acc50cb578e9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d43ee912b06f7054d08d5e13e8b0154",
+    "revision": "906066fd3ed632571c13c1b41ed7600a",
     "url": "/cv/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "a1736de6de5d0571e7d2",
+    "revision": "f9a366666cd14a4a683f",
     "url": "/cv/static/css/main.9c4a9f38.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv/static/js/2.e653429f.chunk.js.LICENSE"
   },
   {
-    "revision": "a1736de6de5d0571e7d2",
-    "url": "/cv/static/js/main.4e329123.chunk.js"
+    "revision": "f9a366666cd14a4a683f",
+    "url": "/cv/static/js/main.8d4c53d8.chunk.js"
   },
   {
     "revision": "d57c9f9ec818ee8bd4d5",
