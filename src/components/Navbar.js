a7e22df1abe974a9474dcb7a7d9c8a3c601ee64f
kamilabartnik <kbartnik@g.pl>
2020-01-13 14:22:04 +0100

    langing page image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Layout, Header, Drawer, Navigation, Content} from 'react-mdl';
 import Main from './main';
+import '../css/navbar.css';
 
 
 function Navbar() {
   return (
     <div className="demo-big-content">
-    <Layout>
-      <Header title="Title" scroll>
-          <Navigation>
-              
-              <a href="/cv/#/myresume/">Resume</a>
-              <a href="/cv/#/aboutme/">About Me</a>
-              <a href="/cv/#/portfolio/">Potrfolio</a>
-              <a href="/cv/#/contact/">Contact</a>
+    <Layout className="navigation">
+      <Header className="navigation header" title="Title" scroll>
+          <Navigation className="navigation bar">
+              <a className="navigation bar a" href="/cv/#/myresume/">Resume</a>
+              <a className="navigation bar a" href="/cv/#/aboutme/">About Me</a>
+              <a className="navigation bar a" href="/cv/#/portfolio/">Potrfolio</a>
+              <a className="navigation bar a" href="/cv/#/contact/">Contact</a>
           </Navigation>
       </Header>
       <Drawer title="Title">
--- src/components/components/landingpage.js
+++ src/components/components/landingpage.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import '../../css/components/landingpage.css';
+import {Grid, Cell} from 'react-mdl';
+import image from '../../assets/KamilaBartnik.jpg'
 
 class Landing extends Component {
   render() {
     return (
-      <div><h1>Landing Page</h1></div>
+      <div className="landing-page">
+        <Grid className="langing-grid">
+          <Cell col={12}>
+            <img 
+              src="cv/src/assets/KamilaBartnik.jpg"
+              alt="avatar"
+              className="avatar-image"
+            />
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
